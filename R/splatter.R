7604a410a9fc43927389b5e2700cb51be7e9cdb2
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2025-06-11 14:19:13 -0500

    add get_groups()

--- R/splatter.R
+++ R/splatter.R
@@ -6,3 +6,16 @@ get_conditions <- function(sim) {
     tapply(conditions.df, conditions.df$sample,
            function(s) unique(s$condition))
 }
+
+#' Get group labels
+#' @param y A `SingleCellExperiment` or `Seurat` object
+get_groups <- function(y, ...) {
+    UseMethod("get_groups")
+}
+
+#' Get the
+get_groups.SingleCellExperiment <- function(y) {
+    x <- colData(y)
+    sg <- paste0(x[["Sample"]], x[["Group"]])
+    factor(sg, labels = seq(1, length(unique(sg))))
+}
